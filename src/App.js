f3f8fe27c1c4fac908f211a7cd4b26f7b701e2da
Sourav Das <souravdas00200@gmail.com>
2023-07-15 22:36:15 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import {Header} from './components/header';
-
+import Article from './components/article';
+import styled from "styled-components";
 function App() {
-  return (
+  return (<Styledsection>
     <div className="App">
       <Header />
       <h1>Hello World</h1>
+      <Article />
     </div>
-  );
+    </Styledsection>);
 }
+const Styledsection = styled.section`
+
+  background-color: black;
+  font-size: 32px;
+  color: white;
+`;
 
 export default App;
---
+++ src/components/article.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// Styled component named StyledButton
+
+function Article() {
+    return (
+        <div className='btns'>
+            <button className="btn">-</button>
+            Update Amount
+            <button className="btn">+</button>
+
+        </div>
+       )
+}
+
+
+export default Article;
